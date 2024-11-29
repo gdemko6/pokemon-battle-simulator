9fe5240ac68f7f6d4a05a249a3407a51bcf66713
Greg Demko <gregory.demko@my.uwrf.edu>
2024-11-29 17:52:11 -0600

    Change hard coded moves and hp to use the PokeAPI

--- src/pages/Battle.jsx
+++ src/pages/Battle.jsx
@@ -1,23 +1,34 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCard from "../components/PokemonCard";
 import "./Battle.css";
 
 function Battle() {
-  const [pokemon1, setPokemon1] = useState({
-    name: "Pikachu",
-    hp: 35,
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-    moves: ["Thunder Shock", "Quick Attack"],
-  });
+  const [pokemon1, setPokemon1] = useState(null);
+  const [pokemon2, setPokemon2] = useState(null);
 
-  const [pokemon2, setPokemon2] = useState({
-    name: "Charizard",
-    hp: 78,
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png",
-    moves: ["Flamethrower", "Dragon Claw"],
-  });
+  useEffect(() => {
+    async function fetchPokemonData(pokemonName, setPokemon) {
+      try {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`
+        );
+        const data = await response.json();
+        setPokemon({
+          name: data.name,
+          hp: data.stats.find((stat) => stat.stat.name === "hp").base_stat,
+          image: data.sprites.other["official-artwork"].front_default,
+          moves: data.moves.slice(0, 2).map((move) => move.move.name),
+        });
+      } catch (error) {
+        console.error(`Error fetching data for ${pokemonName}:`, error);
+      }
+    }
+
+    fetchPokemonData("charizard", setPokemon1);
+    fetchPokemonData("pikachu", setPokemon2);
+  }, []);
+
+  if (!pokemon1 || !pokemon2) return <p>Loading...</p>;
 
   return (
     <div className="battle-page">
