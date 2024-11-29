13286e7587bab14422b4141795eb6687887eee93
Greg Demko <gregory.demko@my.uwrf.edu>
2024-11-28 20:06:04 -0600

    add routing and placeholder pages so that the navbar buttons can reroute users

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+// Placeholder components for pages
+function Home() {
+  return <h1>Welcome to Pokémon Fight Simulator</h1>;
+}
+
+function Battle() {
+  return <h1>Battle Page</h1>;
+}
+
+function History() {
+  return <h1>Battle History</h1>;
+}
+
+function About() {
+  return <h1>About the App</h1>;
+}
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/battle" element={<Battle />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
