358409abdd5e915d4d527df2ab2d888fd6b53cb8
Greg Demko <gregory.demko@my.uwrf.edu>
2024-11-28 20:04:43 -0600

    Add basic home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to Pokémon Fight Simulator</h1>
+      <p>Select two Pokémon to battle and see who wins!</p>
+      <p>Check the history of past battles or learn more about the app.</p>
+    </div>
+  );
+}
+
+export default Home;
